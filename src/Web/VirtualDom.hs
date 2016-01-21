c0383acc105e95c8a6336aa65c8b6137a899ca43
Hans <hans@hanshoglund.se>
2016-01-21 17:15:01 +0000

    Wrap VDOM API

--- src/Web/VirtualDom.hs
+++ src/Web/VirtualDom.hs
@@ -1,6 +1,24 @@
 
 {-# LANGUAGE JavaScriptFFI, CPP, OverloadedStrings #-}
 
+{-
+Low level bindings to virtual-dom.
+
+See https://github.com/Matt-Esch/virtual-dom/tree/master/docs
+
+Supports:
+
+  - @create-element@, @diff@ and @patch@
+  - @VNode@, @VText@ and @Hooks@
+
+Does currently not support:
+
+  - Thunks
+  - Widgets
+  - Namespaced attributes
+
+-}
+
 module Web.VirtualDom
     ( Node
     , Property
@@ -9,7 +27,7 @@ module Web.VirtualDom
     , node
     , text
     , attribute
-    , attributeNS
+    -- , attributeNS
     , on
     , createElement
     , diff
@@ -24,24 +42,52 @@ import Data.JSString
 -- TODO wrap these in newtypes
 
 -- | Virtual DOM node, aka VTree.
+-- https://github.com/Matt-Esch/virtual-dom/blob/master/docs/vtext.md
+-- https://github.com/Matt-Esch/virtual-dom/blob/master/docs/vnode.md
 type Node = JSVal
+
 type Property = JSVal
+
+-- | A DOM node.
 type DOMNode = JSVal
 -- | Difference between DOM nodes, aka PatchObject.
 type Patch = JSVal
 
-node :: JSString -> [Property] -> [Node] -> Node
 foreign import javascript "h$vdom.text($1)"
   text :: JSString -> Node
+
+node :: JSString -> [Property] -> [Node] -> Node
+{-
+basically, we need to call
+  return new VNode(tagName, props, contents, key, namespace);
+
+  tagName is name of node (h1, div etc)
+  props is the list of properties
+    all attributes should be a in the subobject props.attributes
+
+  children is self-explanatory
+  key and namespace are "special" properties
+
+
+-}
+
+
+
+
 property :: JSString -> JSVal -> Property
 attribute :: JSString -> JSString -> Property
-attributeNS :: JSString -> JSString -> JSString -> Property
+
+-- attributeNS :: JSString -> JSString -> JSString -> Property
+-- We need to use a hook to set namespace on attributes
+-- This is mostly used for exotic stuff such as xlink
 
 -- TODO wrap with decoder, allow Options as per below
 on :: JSString -> (JSVal -> IO ()) -> Property
 
-foreign import javascript unsafe "document.body.appendChild($1);"
-  utilPutInBody :: DOMNode -> IO ()
+[node, property, attribute, on] = undefined
+
+
+
 
 foreign import javascript unsafe "h$vdom.createElement($1)"
   createElement :: Node -> IO DOMNode
@@ -52,9 +98,11 @@ foreign import javascript unsafe "h$vdom.diff($1,$2)"
 foreign import javascript unsafe "h$vdom.patch($1,$2)"
   patch :: DOMNode -> Patch -> IO DOMNode
 
+foreign import javascript unsafe "document.body.appendChild($1);"
+  utilPutInBody :: DOMNode -> IO ()
 
 
-[node, property, attribute, attributeNS, on] = undefined
+
 
 
   -- //
