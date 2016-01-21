3f607cb5ea711f0beb0af64f104a44985bacbb34
Hans <hans@hanshoglund.se>
2016-01-21 16:42:38 +0000

    Wrap VDOM API

--- examples/Main.hs
+++ examples/Main.hs
@@ -2,8 +2,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.VirtualDom
+import Control.Concurrent(threadDelay)
 
 main = do
   putStrLn "Hello!"
-  root <- createElement (text "Hello DOM!")
-  utilPutInBody root
+  let node1 = text "Hello DOM!"
+  let node2 = text "Hello different text!"
+  domN <- createElement node1
+  utilPutInBody domN
+  threadDelay 1000000
+
+  p <- diff node1 node2
+  domN2 <- patch domN p
+
+  return ()
