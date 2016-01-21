76bac5ddf8b9ee4207f8f6ddd06b4eaf3b6620b3
Hans <hans@hanshoglund.se>
2016-01-21 16:29:27 +0000

    Wrap VDOM API

--- jsbits/virtual-dom-wrapper.js
+++ jsbits/virtual-dom-wrapper.js
@@ -6,5 +6,46 @@ var patch = require('virtual-dom/vdom/patch');
 var createElement = require('virtual-dom/vdom/create-element');
 var isHook = require("virtual-dom/vnode/is-vhook");
 
-module.exports = { VNode : VNode };
+// type Node
+// type Property
+
+// node : String -> List Property -> List Node -> Node
+function node(name, propertyList, contents)
+{
+  return new VNode(name, [], undefined, undefined);
+}
+
+// text : JSString -> Node
+function text(string) {
+		return new VText(string);
+}
+
+
+
+
+module.exports = {
+  VNode : VNode,
+  VText : VText,
+  diff : diff,
+  patch : patch,
+  createElement : createElement,
+  isHook : isHook,
+
+  node : node,
+  text : text,
+  // property : String -> Json.Value -> Property
+  // attribute : String -> String -> Property
+  // attributeNS : String -> String -> String -> Property
+  //
+  // data Options =
+  //     { stopPropagation : Bool
+  //     , preventDefault : Bool }
+  //
+  // on : String -> Json.Decoder a -> (a -> Signal.Message) -> Property
+
+
+
+
+  dummyToBeRemoved : null
+};
 h$vdom = module.exports;
--- src/Web/VirtualDom.hs
+++ src/Web/VirtualDom.hs
@@ -1,4 +1,47 @@
 
 module Web.VirtualDom
-    (
+    ( Node
+    , Property
+    , DOMNode
+    , Patch
+    , node
+    , text
+    , attribute
+    , attributeNS
+    , on
+    , createElement
+    , diff
+    , patch
     ) where
+
+import GHCJS.Types
+import Data.JSString
+
+-- | Virtual DOM node, aka VTree.
+data Node
+data Property
+data DOMNode
+-- | Difference between DOM nodes, aka PatchObject.
+data Patch
+
+node :: JSString -> [Property] -> [Node] -> Node
+text :: JSString -> Node
+property :: JSString -> JSVal -> Property
+attribute :: JSString -> JSString -> Property
+attributeNS :: JSString -> JSString -> JSString -> Property
+
+-- TODO wrap with decoder, allow Options as per below
+on :: JSString -> (JSVal -> IO ()) -> Property
+
+createElement :: Node -> IO DOMNode
+diff :: Node -> Node -> IO Patch
+patch :: DOMNode -> Patch -> IO DOMNode
+
+[node, text, property, attribute, attributeNS, on, createElement, diff, patch] = undefined
+
+
+  -- //
+  -- // data Options =
+  -- //     { stopPropagation : Bool
+  -- //     , preventDefault : Bool }
+  -- //
